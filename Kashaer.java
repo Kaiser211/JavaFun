66ad2e403c4b3db2e19fb6fd66527a290fa5a96b
Kashaer Abdu <kaiser.kaiser211@gmail.com>
2018-03-05 08:11:59 -0500

    Update Kashaer.java

--- Kashaer.java
+++ Kashaer.java
@@ -1,9 +1,9 @@
 public class Kashaer{
 	public static void main(String[] args) {
 		
-		String name = "Kashaer Abu";
+		String name = "Kashaer Abdu";
 		int age = 40;
-		String hometown = "Urumqi, East Turkistan";
+		String hometown = "Urumqi";
 		
 		System.out.println("My name is " + name);
 		System.out.println("I am "  + age + " years old");
